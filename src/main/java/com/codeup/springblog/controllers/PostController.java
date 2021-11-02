6562b5a8e0a3c4bfbcc288df368ff5dac36656f0
George Tudor <Alexander.George.Tudor@gmail.com>
2021-11-02 12:29:24 -0500

    controller exercises2

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.codeup.springblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,15 +16,22 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     @ResponseBody
-    public String showPostId(@PathVariable long id) {
+    public String show(@PathVariable long id) {
         return "Showing post # " + id;
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String createPost(@PathVariable long id) {
-        return "";
+    public String create(@PathVariable long id) {
+        return "Here is the post create form...";
     }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String insert() {
+        return "New post saved...";
+    }
+
 
 }
 
