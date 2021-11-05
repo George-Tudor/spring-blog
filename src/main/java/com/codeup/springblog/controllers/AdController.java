cf7ba3727deffeb1a52cbac02d5f794a70f15af9
George Tudor <Alexander.George.Tudor@gmail.com>
2021-11-04 19:42:23 -0500

    completed "Build out views for posts" exercise in the Views section

--- src/main/java/com/codeup/springblog/controllers/AdController.java
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -1,27 +1,51 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Ad;
+import com.codeup.springblog.repositories.AdRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class AdController {
 
-    @GetMapping("/ads")
-    @ResponseBody
-    public String showAds() {
-        return "Show all the ads";
+    private final AdRepository adRepository;
+
+    public AdController(AdRepository adRepository) {
+        this.adRepository = adRepository;
     }
 
-    @GetMapping("/ads/{id}")
+
+    @GetMapping("/ads")
     @ResponseBody
-    public String showSingleAd(@PathVariable long id) {
-        return "Showing ad: " + id;
+    public List<Ad> showAds() {
+        return adRepository.findAll();
+    }
+
+//    @GetMapping("/ads/{id}")
+//    @ResponseBody
+//    public String showSingleAd(@PathVariable long id) {
+//        return "Showing ad: " + id;
+//    }
+//
+//    @GetMapping("/ads/{title}")
+//    @ResponseBody
+//    public List<Ad> getByTitle(@PathVariable String title) {
+//        return adRepository.findByTitle(title);
+//    }
+
+    @GetMapping("/ads/{titlePart}")
+    @ResponseBody
+    public List<Ad> getByTitle(@PathVariable String titlePart) {
+        return adRepository.findByTitleLike(titlePart);
     }
 
     @PostMapping("/ads")
     @ResponseBody
-    public String createAd() {
-        return "Creating an ad";
+    public String createAd(@RequestBody Ad newAd) {
+        adRepository.save(newAd);
+        return String.format("Ad created with an id of %s", newAd.getId());
     }
 
     @GetMapping("/color/{color}")
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,23 +1,42 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String showPosts() {
-        return "posts index page";
+    public String showPosts(Model model) {
+        Post post1 = new Post("Xbox", "New in the box, $300");
+        Post post2 = new Post("Ipad", "Ipad 2, 16gig, $100");
+        Post post3 = new Post("Laptop", "Old laptop, works, $100");
+        Post post4 = new Post("Nissan Maxima", "1989, Runs well, cold AC, $2000");
+        Post post5 = new Post("Skateboard", "Used, new wheels $30");
+        List<Post> posts = new ArrayList<>();
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+        posts.add(post4);
+        posts.add(post5);
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
 
-    @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String show(@PathVariable long id) {
-        return "Showing post # " + id;
+    @GetMapping("/posts/{title}")
+    public String show(@PathVariable String title, Model model) {
+        Post post = new Post("Xbox", "New in the box, $300");
+        model.addAttribute("title", post.getTitle());
+        model.addAttribute("body", post.getBody());
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
@@ -31,7 +50,6 @@ public class PostController {
     public String insert() {
         return "New post saved...";
     }
-
 
 }
 
